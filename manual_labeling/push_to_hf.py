39903311db5b64e232d9a0c2493528b7b7a517b2
ANYANTUDRE <121746248+ANYANTUDRE@users.noreply.github.com>
2024-10-09 10:46:32 +0100

    update

--- manual_labeling/push_to_hf.py
+++ manual_labeling/push_to_hf.py
@@ -11,7 +11,7 @@ dataset = Dataset.load_from_disk(dataset_path)
 
 api = HfApi(token=HF_TOKEN)
 
-repo_id = "ArissBandoss/proverbes-moore-vol2"
+repo_id = "ArissBandoss/bible-moore-audios"
 dataset.push_to_hub(repo_id)
 
 print(f"Dataset successfully pushed to Hugging Face Hub at: https://huggingface.co/{repo_id}")
